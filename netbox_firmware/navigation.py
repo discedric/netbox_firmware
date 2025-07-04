39a948bf46cdb29670d434cbeb939b0f94b455f9
Bart Van der Biest <bart@zimmo.be>
2025-07-04 11:11:30 +0200

    Fix Navigation with permissions. - Include button config in Menu Items - Change layout - Fix permissions

--- netbox_firmware/navigation.py
+++ netbox_firmware/navigation.py
@@ -1,90 +1,96 @@
 from netbox.plugins import PluginMenu, PluginMenuItem, PluginMenuButton, get_plugin_config
 
-firmware_buttons = [
-    PluginMenuButton(
-        link='plugins:netbox_firmware:firmware_add',
-        title='Add',
-        icon_class='mdi mdi-plus-thick',
-        permissions=["netbox_firmware.add_firmware"],
-    ),
-    PluginMenuButton(
-        link='plugins:netbox_firmware:firmware_bulk_import',
-        title='Import',
-        icon_class='mdi mdi-upload',
-        permissions=["netbox_firmware.import_firmware"],
-    ),
-]
-firmware_assignments_buttons = [
-    PluginMenuButton(
-        link='plugins:netbox_firmware:firmwareassignment_add',
-        title='Add',
-        icon_class='mdi mdi-plus-thick',
-        permissions=["netbox_firmware.add_firmware_assignment"],
-    ),
-    PluginMenuButton(
-        link='plugins:netbox_firmware:firmwareassignment_bulk_import',
-        title='Import',
-        icon_class='mdi mdi-upload',
-        permissions=["netbox_firmware.aimport_firmware_assignment"],
-    ),
-]
+### Firmware ###
+
 firmware_items = (
     PluginMenuItem(
         link='plugins:netbox_firmware:firmware_list',
         link_text='Firmwares',
         permissions=["netbox_firmware.view_firmware"],
-        buttons= firmware_buttons,
+        buttons= [
+            PluginMenuButton(
+                link='plugins:netbox_firmware:firmware_add',
+                title='Add',
+                icon_class='mdi mdi-plus-thick',
+                permissions=["netbox_firmware.add_firmware"],
+            ),
+            PluginMenuButton(
+                link='plugins:netbox_firmware:firmware_bulk_import',
+                title='Import',
+                icon_class='mdi mdi-upload',
+                permissions=["netbox_firmware.add_firmware"],
+            ),
+        ],
     ),
     PluginMenuItem(
         link='plugins:netbox_firmware:firmwareassignment_list',
         link_text='Firmware Assignments',
-        permissions=["netbox_firmware.view_firmware_assignment"],
-        buttons= firmware_assignments_buttons,
+        permissions=["netbox_firmware.view_firmwareassignment"],
+        buttons= [
+            PluginMenuButton(
+                link='plugins:netbox_firmware:firmwareassignment_add',
+                title='Add',
+                icon_class='mdi mdi-plus-thick',
+                permissions=["netbox_firmware.add_firmwareassignment"],
+            ),
+            PluginMenuButton(
+                link='plugins:netbox_firmware:firmwareassignment_bulk_import',
+                title='Import',
+                icon_class='mdi mdi-upload',
+                permissions=["netbox_firmware.add_firmwareassignment"],
+            ),
+        ],
     ),
 )
 
-bios_buttons = [
-    PluginMenuButton(
-        link='plugins:netbox_firmware:bios_add',
-        title='Add',
-        icon_class='mdi mdi-plus-thick',
-        permissions=["netbox_firmware.add_bios"],
-    ),
-    PluginMenuButton(
-        link='plugins:netbox_firmware:bios_bulk_import',
-        title='Import',
-        icon_class='mdi mdi-upload',
-        permissions=["netbox_firmware.import_bios"],
-    ),
-]
-bios_assignments_buttons = [
-    PluginMenuButton(
-        link='plugins:netbox_firmware:biosassignment_add',
-        title='Add',
-        icon_class='mdi mdi-plus-thick',
-        permissions=["netbox_firmware.add_bios_assignment"],
-    ),
-    PluginMenuButton(
-        link='plugins:netbox_firmware:biosassignment_bulk_import',
-        title='Import',
-        icon_class='mdi mdi-upload',
-        permissions=["netbox_firmware.import_bios_assignment"],
-    ),
-]
+
+### BIOS ###
+
 bios_items = (
     PluginMenuItem(
         link='plugins:netbox_firmware:bios_list',
         link_text='BIOS',
         permissions=["netbox_firmware.view_bios"],
-        buttons= bios_buttons,
+        buttons= [
+            PluginMenuButton(
+                link='plugins:netbox_firmware:bios_add',
+                title='Add',
+                icon_class='mdi mdi-plus-thick',
+                permissions=["netbox_firmware.add_bios"],
+            ),
+            PluginMenuButton(
+                link='plugins:netbox_firmware:bios_bulk_import',
+                title='Import',
+                icon_class='mdi mdi-upload',
+                permissions=["netbox_firmware.add_bios"],
+            ),
+        ],
     ),
     PluginMenuItem(
         link='plugins:netbox_firmware:biosassignment_list',
         link_text='BIOS Assignments',
-        permissions=["netbox_firmware.view_bios_assignment"],
-        buttons= bios_assignments_buttons,
+        permissions=["netbox_firmware.view_biosassignment"],
+        buttons= [
+            PluginMenuButton(
+                link='plugins:netbox_firmware:biosassignment_add',
+                title='Add',
+                icon_class='mdi mdi-plus-thick',
+                permissions=["netbox_firmware.add_biosassignment"],
+            ),
+            PluginMenuButton(
+                link='plugins:netbox_firmware:biosassignment_bulk_import',
+                title='Import',
+                icon_class='mdi mdi-upload',
+                permissions=["netbox_firmware.add_biosassignment"],
+            ),
+        ],
     ),
 )
+
+### -> Add new Menu Items above this line if needed.
+
+### Menu ###
+
 
 if get_plugin_config('netbox_firmware', 'top_level_menu'):
     menu = PluginMenu(
